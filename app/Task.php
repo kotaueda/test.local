5633820729bb53498a469d02266d53fdc15b84ab
vagrant <vagrant@homestead>
2020-04-08 07:45:29 +0000

    アクセサを使って状態ごとに色分けする

--- app/Task.php
+++ app/Task.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     // statusのlabelの値を文字列への変更し定義
+    // 配列にクラスを追加する
     const STATUS = [
-        1 => [ 'label' => '未着手'],
-        2 => [ 'label' => '着手中'],
-        3 => [ 'label' => '完了'],
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
     ];
 
     // statusの文字列を取得するためのメソッド
@@ -26,5 +27,20 @@ class Task extends Model
 
         // statusカラムの値へアクセス
         return self::STATUS[$status]['label'];
+    }
+
+    // statusの色分けを取得するためのメソッド
+    public function getStatusClassAttribute()
+    {
+        // statusカラムの値を取得
+        $status = $this->attributes['status'];
+
+        // isset関数で変数に値が入っているかをチェックする
+        if (!isset(self::STATUS[$status])) {
+            return ''; //　空だった場合は空文字を返す
+        }
+
+        // statusカラムの値へアクセス
+        return self::STATUS[$status]['class'];
     }
 }
