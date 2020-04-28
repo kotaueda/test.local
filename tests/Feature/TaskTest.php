6a940f7200fa49114d259edf29177d73d68b2207
vagrant <vagrant@homestead>
2020-04-28 07:31:39 +0000

    状態のバリデーションをテストする

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -115,4 +115,39 @@ class TaskTest extends TestCase
             'due_date' => '期限日 には日付を入力してください。',
         ]);
     }
+
+    /**
+     * 状態が定義された値の場合はタスク一覧ページへリダイレクトする
+     * @test
+     */
+    public function status_should_be_within_defined_numbers()
+    {
+        $this->seed('TasksTableSeeder');
+
+        $response = $this->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999, // 不正なデータ（数値）
+        ]);
+
+        $response->assertSessionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
+        ]);
+    }
+
+    /**
+     * 状態が定義された値の場合はエラーを返す
+     * @test
+     */
+    public function status()
+    {
+        $response = $this->post('/folders/1/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 1,
+        ]);
+
+        $response->assertRedirect('/folders/1/tasks');
+    }
+
 }
